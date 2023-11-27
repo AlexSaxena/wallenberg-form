e224744f4265486492d88e6022c304dcf9377573
AlexSaxena <saxenaalex@gmail.com>
2023-11-27 12:10:58 +0100

    Adds 404 error page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NyElevForm from './pages/NyElevForm';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/hem" element={<Home />} />
           <Route path="/form" element={<NyElevForm />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </>
