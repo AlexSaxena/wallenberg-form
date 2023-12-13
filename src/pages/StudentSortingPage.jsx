4dc9ceecfe0f62fbb209a817aee0cbea22073e81
Alex Saxena <saxenaalex@gmail.com>
2023-12-13 22:36:08 +0100

    Implements delete functionality for selected student

--- src/pages/StudentSortingPage.jsx
+++ src/pages/StudentSortingPage.jsx
@@ -18,6 +18,20 @@ const StudentSortingPage = () => {
     setstudents(res.data.showStudents);
   };
 
+  const deleteStudent = async (_id) => {
+    console.log("Deleted");
+    // Delete the selected Student
+    const res = await axios.delete(`http://localhost:3001/showStudents/${_id}`);
+    console.log(res);
+
+    // Update State
+    const newStudents = [...students].filter((student) => {
+      return student._id !== _id;
+    });
+
+    setstudents(newStudents);
+  };
+
   return (
     <div className="w-screen h-screen flex flex-col items-center bg-gray-200">
       <Header />
@@ -27,7 +41,18 @@ const StudentSortingPage = () => {
         <h2>All Students</h2>
         {students &&
           students.map((student) => {
-            return <p key={student._id}>{student.name}</p>;
+            return (
+              <div key={student._id}>
+                {student.name} {student.city}
+                <button
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-red"
+                  type="button"
+                  onClick={() => deleteStudent(student._id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
           })}
       </div>
     </div>
