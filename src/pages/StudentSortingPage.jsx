8f87222f20c02397139f7ae8e517af04b5bc633a
Alex Saxena <saxenaalex@gmail.com>
2023-12-13 15:49:49 +0100

    Adds functionality to fetch Student Data from API

--- src/pages/StudentSortingPage.jsx
+++ src/pages/StudentSortingPage.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "../components/Header";
 
 const StudentSortingPage = () => {
   const [students, setstudents] = useState(null);
 
-  const fetchStudents = () => {
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const fetchStudents = async () => {
     // Fetch Students
+    const res = await axios.get("http://localhost:3001/showStudents");
+    console.log(res.data.showStudents);
     // Set State of Students
+
+    setstudents(res.data.showStudents);
   };
 
   return (
@@ -15,6 +23,13 @@ const StudentSortingPage = () => {
       <Header />
       <h1>Sorting Page</h1>
       <h2>Page for finding students by querying API</h2>
+      <div>
+        <h2>All Students</h2>
+        {students &&
+          students.map((student) => {
+            return <p key={student._id}>{student.name}</p>;
+          })}
+      </div>
     </div>
   );
 };
