0e0a6d38e96762eefaf3b4c4059a84830532567e
Alex Saxena <saxenaalex@gmail.com>
2023-12-19 15:03:48 +0100

    Adds Axios base url for localhost server port

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />,
-)
+// Adds base url for localhost server port
+axios.defaults.baseURL = "http://localhost:3001";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
