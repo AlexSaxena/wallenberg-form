39ab80efe4063ae5c74410b425e2957c9165c214
Alex Saxena <saxenaalex@gmail.com>
2023-12-20 13:07:40 +0100

    adds formState in authStore

--- stores/authStore.jsx
+++ stores/authStore.jsx
@@ -1,6 +1,21 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const authStore = create((set) => ({}));
+const authStore = create((set) => ({
+  loginForm: {
+    username: "",
+    password: "",
+  },
+
+  updateLoginForm: (e) => {
+    const { name, value } = e.target;
+
+    set((state) => {
+      return {
+        loginForm: { ...state.loginForm, [name]: value },
+      };
+    });
+  },
+}));
 
 export default authStore;
